e1dca10aa88a6b123189deb17c69eaf65b3ff81b
Joe Becher <jwbecher@gmail.com>
2019-05-09 20:34:17 -0400

    create function

--- mcmanager.py
+++ mcmanager.py
@@ -2,6 +2,19 @@
 
 from urllib.request import urlopen, Request
 from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def fetch_page(url, user_agent):
+    """Fetch a web page and return the contents."""
+    try:
+        req = Request(url,
+                      headers={'User-Agent': user_agent})
+        with urlopen(req) as r:
+            return(r.read())
+    except HTTPError as err:
+        print('whoops! ' + err.msg)
+        print(err.headers)
 
 
 def main():
@@ -9,17 +22,12 @@ def main():
 
     admin_email = "<jwbecher@drazisil.com>"
 
-    try:
-        user_agent = "MCManager {}".format(admin_email)
-        print(user_agent)
-        req = Request(url,
-                      headers={'User-Agent': user_agent})
-        with urlopen(req) as r:
-            print(r.info())
-            print(r.read())
-    except HTTPError as err:
-        print('whoops! ' + err.msg)
-        print(err.headers)
+    user_agent = "MCManager {}".format(admin_email)
+
+    page_contents = fetch_page(url, user_agent)
+
+    soup = BeautifulSoup(page_contents, 'html.parser')
+    print(soup.title.string)
 
 
 if __name__ == "__main__":
